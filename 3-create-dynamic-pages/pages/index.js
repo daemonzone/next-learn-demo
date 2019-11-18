58eac276c31d266c12fef3beae522abed406f6ce
davide.villani <davide.villani@altran.it>
2019-11-18 17:18:40 +0100

    + Forked demo project + Gone through lessone 1-6

--- 3-create-dynamic-pages/pages/index.js
+++ 3-create-dynamic-pages/pages/index.js
@@ -1,9 +1,22 @@
-import Layout from '../components/MyLayout.js'
+import Layout from '../components/MyLayout';
+import Link from 'next/link';
 
-export default function Index() {
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+	  <a>{props.title}</a>
+    </Link>
+  </li>
+);
+export default function Blog() {
   return (
     <Layout>
-      <p>Hello Next.js</p>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" date="today" />
+        <PostLink title="Learn Next.js is awesome"  date="yesterday" />
+        <PostLink title="Deploy apps with Zeit"  date="tomorrow" />
+      </ul>
     </Layout>
-  )
+  );
 }
